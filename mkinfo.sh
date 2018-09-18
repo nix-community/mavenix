9e468e513ed65319fd1c9dd7b49ae68ee00fbf2e
Christopher Fredén <christopher.freden@seal-software.com>
2018-09-18 21:12:44 +0200

    Big UX refactor

--- mkinfo.sh
+++ mkinfo.sh
@@ -1,22 +1,11 @@
-declare -A repos
-
-set_repos() {
-  while test "$1";do
-    repos["$1="]="$2"
-    shift 2
-  done
-}
-
 echo >&2 "RUNING MAVEN INSTALL" >&2
-mvn_ >&2 install -B -Dmaven.repo.local=${TMP_REPO} #|| echo -n
+mvn_ >&2 install -B -Dmaven.repo.local=${TMP_REPO}
 
 echo >&2 "GETTING PROJECT INFO"
-#cp -r "${TMP_REPO}" "${TMP_REPO}.exec"
 
 # XXX the maven-exec plugin will be put in the generated repo which might be
 # unwanted.
 proj="$(mvn_ -q --non-recursive \
-  -Dmaven.repo.local=${TMP_REPO} \
   org.codehaus.mojo:exec-maven-plugin:1.3.1:exec \
   -Dexec.executable="echo" -Dexec.args='${project.groupId} ${project.artifactId} ${project.version}')"
 groupId="$(cut -d' ' -f1 <<<"$proj")"
@@ -24,7 +13,6 @@ artifactId="$(cut -d' ' -f2 <<<"$proj")"
 version="$(cut -d' ' -f3 <<<"$proj")"
 
 submodules="$(mvn_ -q \
-  -Dmaven.repo.local=${TMP_REPO} \
   org.codehaus.mojo:exec-maven-plugin:1.3.1:exec \
   -Dexec.executable="echo" \
   -Dexec.args='-n ,\\n
@@ -42,16 +30,6 @@ mvn_ >&2 dependency:go-offline -B -Dmaven.repo.local=${TMP_REPO}
 # Maven 3.0.5
 #mvn >&2 org.apache.maven.plugins:maven-dependency-plugin:2.6:go-offline -Dmaven.repo.local=${TMP_REPO}
 
-echo >&2 "RESOLVING MAVEN REPOSITORIES"
-# Maven 3.3.9
-set_repos $(mvn_ -o \
-  dependency:list-repositories -Dmaven.repo.local=${TMP_REPO} 2>&- \
-  | grep -Eo '(id: |url: ).*$' | sed 's|[^ ]*||')
-# Maven 3.0.5
-#set_repos $(mvn -o \
-#   org.apache.maven.plugins:maven-dependency-plugin:2.6:list-repositories \
-#   --settings $settings -Dmaven.repo.local=${TMP_REPO} 2>&1 \
-#  | grep -Eo '(id: |url: ).*$' | sed 's|[^ ]*||')
 
 echo >&2 "CREATING OUTPUT"
 (
@@ -66,47 +44,46 @@ echo -n $submodules \
   | sed 's/,//;s|\\\\q'$PWD'|".|g;s/\\\\s/  /g;s/\\\\n/\n /g;s/\\\\q/"/g'
 echo -n  "
   ],
-
   \"deps\": ["
 ( cd $TMP_REPO
 remotes="$(find . -type f -name "*.repositories" | sed 's|^\./||' | sort)"
+sep=""
 for remote in $remotes; do
   dir="$(dirname "$remote")"
-  #test $(find "$dir" -type f -name "*.jar" | wc -l) -gt 1 && continue
   files="$(find "$dir" -type f ! -name "*.repositories" ! -name "*.sha1" \
     | grep -v '^#' "$remote" | sed "s|^|$dir/|")"
   for file_ in $files; do
     file=$(echo $file_ | cut -d '>' -f1)
     # Maven 3.0.5 for 3.3.9 use $file instead of $file_real
     file_real=$(echo $(echo $file | sed 's/-SNAPSHOT\./-[0-9]*\./'))
-    repo=$(echo $file_ | cut -d '>' -f2)
-    test "${repos[$repo]}" || continue
-    # TODO : Remove `url` to make dependencies registry independent
-    echo -n "$sep
-    {
+    repo=$(echo $file_ | cut -d '>' -f2 | sed 's/=$//')
+    test "$repo" || continue
+    echo -n "$sep{
       \"path\": \"$file_real\",
-      \"url\": \"${repos[$repo]}/$file_real\",
       \"sha1\": \"$(grep -Eo '[0-9a-zA-Z]{40}' < $file_real.sha1)\"
     }"
-    sep=,
+    sep=", "
   done
 done
 
+echo -n "
+  ],
+  \"metas\": ["
 # XXX: is this needed? Yes, for transitive deps
 metafiles="$(find . -type f -name "maven-metadata-*.xml"  | sed 's|^\./||' | sort)"
+sep=""
 for file in $metafiles; do
-  repo=$(basename $file | sed 's/^maven-metadata-//;s/\.xml$/=/')
-  test "${repos[$repo]}" || continue
-  echo -n ",
-    {
-      \"path\": \"$file\",
-      \"url\": \"${repos[$repo]}/$(dirname $file)/maven-metadata.xml\",
-      \"sha1\": \"$(sha1sum $file | grep -Eo '[0-9a-zA-Z]{40}')\"
+  repo=$(basename $file | sed 's/^maven-metadata-//;s/\.xml$//')
+  test "$repo" || continue
+  echo -n "$sep{
+      \"path\": \"$(dirname $file)\",
+      \"content\": \"$(sed ':a;N;$!ba;s/\n/\\n/g;s/\"/\\\"/g' $file)\"
     }"
+  sep=", "
 done
 )
 echo -n "
   ]
 }
 "
-) > $output
+) > "$output"
