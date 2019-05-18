0afccf2426ae4795fbc2d470772d582f8442502b
Christopher Fredén <me@icetan.org>
2019-05-19 00:32:15 +0200

    Fix misspelled tarball URLs

--- mavenix.nix
+++ mavenix.nix
@@ -1,6 +1,6 @@
 {
   pkgs ? import (fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/tarballs/18.09";
+    url = "https://github.com/NixOS/nixpkgs/tarball/18.09";
     sha256  = "1ib96has10v5nr6bzf7v8kw7yzww8zanxgw2qi1ll1sbv6kj6zpd";
   }) {},
 }:
