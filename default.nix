581b44039b90a0562d4c69bf860944e8335db3a3
Ilja Tkachuk <tkachuk.labs@gmail.com>
2020-07-25 20:05:44 +0300

    fetchGit -> fetchTarball

--- default.nix
+++ default.nix
@@ -1,9 +1,5 @@
 {
-  pkgs ? (import (fetchGit {
-    url = "https://github.com/NixOS/nixpkgs-channels";
-    ref = "nixos-19.09";
-    rev = "c75de8bc12cc7e713206199e5ca30b224e295041";
-  }) {}),
+  pkgs ? (import (fetchTarball "https://github.com/NixOS/nixpkgs-channels/tarball/c75de8bc12cc7e713206199e5ca30b224e295041";) {}),
   mavenixLib ? import ./mavenix.nix { inherit pkgs; },
 }: with pkgs;
 
